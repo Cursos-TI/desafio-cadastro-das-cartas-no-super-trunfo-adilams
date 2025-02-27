f5c991f8db72a791fb0146ec1fcd3ad0aaa0893d
adilams <adilamacedo3@gmail.com>
2025-02-27 23:55:24 +0000

    criei o printf

---
+++ super_trunfo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+    int população;
+    int Número de Pontos Turísticos;
+    int float Área (em km²);
+    int float PIB;
+    char nome da Cidade [];
+    char estado [];
+    char Código da Carta [];
+
+
+
+
+
+
+
+
+
+
+
+}
